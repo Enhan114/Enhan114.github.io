f06e812fdd44039e62bc72ec9635c623bf82590c
killmyworld <sinianjierban5hao@163.com>
2026-06-09 11:43:23 +0800

    Fix extension: add window-level dispatch, debug logging

--- ext/content.js
+++ ext/content.js
@@ -2,8 +2,22 @@
 // Dispatches native KeyboardEvents on the document so the
 // app's keyboard shortcut system picks them up naturally.
 
+console.log("[AuraExt] content script loaded for", location.href);
+
 function fireKey(key, code, ctrl, shift) {
+  console.log("[AuraExt] fireKey:", { key, code, ctrl, shift });
   document.dispatchEvent(new KeyboardEvent("keydown", {
+    key: key,
+    code: code,
+    ctrlKey: !!ctrl,
+    shiftKey: !!shift,
+    metaKey: false,
+    altKey: false,
+    bubbles: true,
+    cancelable: true,
+  }));
+  // Also dispatch on window directly for robustness
+  window.dispatchEvent(new KeyboardEvent("keydown", {
     key: key,
     code: code,
     ctrlKey: !!ctrl,
@@ -22,6 +36,7 @@ const handlers = {
 };
 
 chrome.runtime.onMessage.addListener(function(msg) {
+  console.log("[AuraExt] received command:", msg.command);
   var handler = handlers[msg.command];
   if (handler) handler();
 });
